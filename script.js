02711540fbcc53b998b7c25d370b36c5b4430f08
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 13:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const p = document.getElementById("timer")
+const now = new Date();
+
+
+        const day = now.getDay();
+        const month = now.getMonth()+1;
+        const year = now.getFullYear();
+
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+p.innerText = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
